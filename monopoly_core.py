763ffd2b84bf02803a1b5c4130e8a1846ae600e9
Your Name <sanzhar.learn@gmail.comccd>
2025-11-17 14:11:12 +0100

    add gitignore and core file

--- monopoly_core.py
+++ monopoly_core.py
@@ -15,25 +15,25 @@ def create_initial_state(num_players = 2):
 
 
 def get_current_player(state):
-    """Return the dict of the current player"""
+    #Return the dict of the current player
     pass
 
 
 def list_legal_commands(state):
-    """Return all legal actions the current player may take"""
+    #Return all legal actions the current player may take
     pass
 
 
 def apply_command(state, command, payload=None):
-    """Apply a Monopoly command and modify state"""
+    #Apply a Monopoly command and modify state
     pass
 
 
 def is_game_over(state):
-    """Determine if the game has ended"""
+    #Determine if the game has ended
     pass
 
 
 def serialize_state(state):
-    """Return a JSON-safe dict representing the entire game state"""
+    #Return a JSON-safe dict representing the entire game state
     pass
